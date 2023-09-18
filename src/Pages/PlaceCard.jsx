a11c641150e59a6e34b7ef8bae2890325757decb
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-09-19 00:32:48 +0600

    Implement place card rating system.

--- src/Pages/PlaceCard.jsx
+++ src/Pages/PlaceCard.jsx
@@ -6,23 +6,36 @@ import { Card, Image } from 'react-bootstrap';
 import { FcBookmark, FcShare } from "react-icons/fc";
 import { FaRegStar, FaStar, FaEye } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import Rating from 'react-rating';
 
 // eslint-disable-next-line react/prop-types
 const PlaceCard = ({ place }) => {
-    const { title, placeName, imageUrl, Rating, date, id } = place;
+    const { title, placeName, imageUrl, rating, date, id } = place;
     return (
-        <div className="card card-side bg-base-100 shadow-xl m-6 flex justify-between gap-20 ">             
+        <div className="card card-side bg-base-100 shadow-xl m-6 flex justify-between gap-20 ">
             <div className=''>
                 <figure><img className='p-3 h-72 w-96 ' src={imageUrl} alt="Movie" /></figure>
             </div>
             <div className="card-body ">
                 <h2 className="card-title">{placeName}</h2>
                 <p>{title}</p>
-                <div>
-                    <p>{Rating}</p>
-                </div>
-                <div className="card-actions justify-end">
-                    <Link to={`/place/${id}`} className="btn btn-primary">More Details</Link>
+                <p><small>{moment(date).format('yyyy-MM-D')}</small></p>
+                <div className="card-actions  flex justify-between">
+                    {/* <p>{Rating}</p>                 */}
+                    <div className='flex gap-4'>
+                        <Rating
+                         placeholderRating={rating}
+                         readonly
+                         emptySymbol={<FaRegStar></FaRegStar>}
+                         placeholderSymbol={<FaStar className='text-warning'></FaStar>}
+                         className='icon'
+                         fullSymbol={<FaStar></FaStar>}
+                        >
+
+                        </Rating>
+                        {rating}
+                    </div>
+                    <Link to={`/place/${id}`} className="btn btn-primary">More Details</Link>                
                 </div>
             </div>
         </div>
--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
+        <div className=''>
             <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
                 <nav className="grid grid-flow-col gap-4">
                     <a className="link link-hover">About us</a>
